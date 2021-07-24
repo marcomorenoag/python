03344e8eb05e7fc71f106d3e823f744b58b09325
Marco Moreno <marcomoreno@wineadvisor.mx>
2021-07-24 13:16:21 -0500

    feat(frameworks): :sparkles: add part 6 for FlaskAPI
    Implement blog post delete last 10 items using Queue and Stack

--- frameworks/flask/FlaskAPI/server.py
+++ frameworks/flask/FlaskAPI/server.py
@@ -11,6 +11,8 @@ import random
 import linked_list
 import hash_table
 import binary_search_tree
+import custom_q
+import stack
 
 
 # Initialize our Flask app, behind scenes Flask use a WSGI to connect Python App with the server
@@ -148,7 +150,7 @@ def create_blog_post(user_id):
 
 
 @app.route('/blog_post/<blog_post_id>', methods = ['GET'])
-def get_all_blog_posts(blog_post_id):
+def get_one_blog_post(blog_post_id):
   blog_posts = BlogPost.query.all()
   random.shuffle(blog_posts) # random order due query.all() retrieve them ascending, this increases chances of have a balanced BST
   bst = binary_search_tree.BinarySearchTree()
@@ -164,13 +166,40 @@ def get_all_blog_posts(blog_post_id):
     return jsonify({ 'message': 'Post not found'})
   return jsonify(post)
 
-@app.route('/blog_post/<blog_post_id>', methods = ['GET'])
-def get_one_blog_post(blog_post_id):
-  pass
+@app.route('/blog_post/numeric_body', methods = ['GET'])
+def get_numeric_post_bodies():
+  blog_posts = BlogPost.query.all()
+  q = custom_q.Queue()
+  for post in blog_posts:
+    q.enqueue(post)
+  return_list = []
+  for _ in range(len(blog_posts)):
+    post = q.dequeue()
+    numeric_body = 0
+    for char in post.data.body:
+      numeric_body += ord(char)
+    post.data.body = numeric_body
+    return_list.append(
+      {
+        'id': post.data.id,
+        'title': post.data.title,
+        'body': post.data.body,
+        'user_id': post.data.user_id,
+      }
+    )
+  return jsonify(return_list), 200
 
-@app.route('/blog_post/<blog_post_id>', methods = ['DELETE'])
-def delete_blog_post(blog_post_id):
-  pass
+@app.route('/blog_post/delete_last_10', methods = ['DELETE'])
+def delete_last_10():
+  blog_posts = BlogPost.query.all()
+  s = stack.Stack()
+  for post in blog_posts:
+    s.push(post)
+  for _ in range(10):
+    post_to_delete = s.pop()
+    db.session.delete(post_to_delete.data)
+    db.session.commit()
+  return jsonify({ 'message': 'success' })
 
 
 if __name__ == '__main__':
